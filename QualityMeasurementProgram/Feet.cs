10f1b8c6b5c632b61cad448f5f44d22919524acd
unknown <vishalpwaman123@gmail.com>
2020-06-19 20:45:04 -0700

    Added: Checked Equality of the Two Feets

---
+++ QualityMeasurementProgram/Feet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+
+    public class Feet
+    {
+        private double value;
+
+        public Feet(double value)
+        {
+            this.value = value;
+        }
+
+        public override bool Equals(object o)
+        {
+            // It checks for the Object
+            if (this == o)
+            {
+                return true;
+            }
+
+            // It checks for object if null or object type is not equal then return false
+            if (o == null || !this.GetType().Equals(o.GetType()))
+            {
+                return false;
+            }
+
+            Feet feet = (Feet)o;
+            return value.CompareTo(feet.value) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(value);
+        }
+    }
+}
---
+++ QualityMeasurementProgram/QualityMeaurementException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+    public class QualityMeaurementException : Exception
+    {
+        public ExceptionType Type;
+
+        public string message;
+        public QualityMeaurementException(string message, ExceptionType type)
+        {
+            this.Type = type;
+            this.message = message;
+        }
+
+        public enum ExceptionType
+        {
+            INVALID_ARGUMENT
+        }
+    }
+
+}
