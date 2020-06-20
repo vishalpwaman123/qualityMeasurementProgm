907aae953189e8ba49c2fd7d42f46c02ce7d0520
unknown <vishalpwaman123@gmail.com>
2020-06-19 20:50:31 -0700

    Added: Invalid Argument & Exception Handling

---
+++ QualityMeasurementProgram/Feet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+    public class Feet
+    {
+        private double value;
+
+        public Feet(double value)
+        {
+            this.value = value;
+        }
+
+        public Feet(string value)
+        {
+            throw new QualityMeaurementException("Invalid Argument", QualityMeaurementException.ExceptionType.INVALID_ARGUMENT);
+        }
+        public override bool Equals(object o)
+        {
+            // It checks for the Object
+            if (this == o)
+            {
+                return true;
+            }
+
+            // It checks for object if null or object type is not equal then return false
+            if (o == null || !this.GetType().Equals(o.GetType()))
+            {
+                return false;
+            }
+
+            Feet feet = (Feet)o;
+            return value.CompareTo(feet.value) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(value);
+        }
+    }
+}
--- QualityMeasurementTest/UnitTest1.cs
+++ QualityMeasurementTest/UnitTest1.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using QualityMeasurementProgram;
 namespace QualityMeasurementTest
 {
     public class Tests
@@ -10,9 +10,36 @@ namespace QualityMeasurementTest
         }
 
         [Test]
-        public void Test1()
+        public void GivenZeroFeetAndZeroFeet_ShouldReturnsEqual()
         {
-            Assert.Pass();
+            try
+            {
+
+                Feet firstFeet = new Feet(0.0);
+                Feet secondFeet = new Feet(0.0);
+                Assert.AreEqual(firstFeet, secondFeet);
+            }
+            catch (QualityMeaurementException e)
+            {
+                Assert.AreEqual(QualityMeaurementException.ExceptionType.INVALID_ARGUMENT, e.Type);
+            }
         }
+
+        [Test]
+        public void GivenStringFeetAndZeroFeet_WhenInvalidArgument_ShouldReturnsThrowException()
+        {
+            try
+            {
+
+                Feet firstFeet = new Feet("0.0");
+                Feet secondFeet = new Feet(0.0);
+                Assert.AreEqual(firstFeet, secondFeet);
+            }
+            catch (QualityMeaurementException e)
+            {
+                Assert.AreEqual(QualityMeaurementException.ExceptionType.INVALID_ARGUMENT, e.Type);
+            }
+        }
+
     }
 }
