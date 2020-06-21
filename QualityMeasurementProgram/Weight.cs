cc33d30c93b5a28a0e90c2b9dfbf5e4408c4b111
unknown <vishalpwaman123@gmail.com>
2020-06-21 01:51:03 -0700

    Added: TC_7.1 Given One KG and One KG, Value Checked Returns Equal

---
+++ QualityMeasurementProgram/Weight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+    public class Weight
+    {
+        public enum Unit
+        {
+            KILOGRAMS,
+            GRAMS,
+            GRAMStoKILOGRAMS
+        }
+
+        public Weight()
+        {
+
+        }
+        public double ConvertValueToKiloGrams(Unit unit, double value)
+        {
+            try
+            {
+                if (unit.Equals(Unit.GRAMStoKILOGRAMS))
+                {
+                    return value / 1000;
+                }
+
+                return value;
+            }
+            catch (QualityMeaurementException e)
+            {
+                throw new QualityMeaurementException(e.Message, QualityMeaurementException.ExceptionType.INVALID_ARGUMENT);
+            }
+        }
+
+    }
+}
---
+++ QualityMeasurementTest/WeightTestCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using QualityMeasurementProgram;
+
+namespace NUnitTestQuantityMeasurement
+{
+    class WeightTestCase
+    {
+        private Weight kiloGram = null;
+        private Weight gram = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.kiloGram = new Weight();
+            this.gram = new Weight();
+        }
+
+        [Test]
+        public void Given0KiloGramsAnd0KiloGrams_ShouldReturnsEqual()
+        {
+            double firstKiloGramsValue = kiloGram.ConvertValueToKiloGrams(Weight.Unit.KILOGRAMS, 0.0);
+            double secondKiloGramsValue = kiloGram.ConvertValueToKiloGrams(Weight.Unit.KILOGRAMS, 0.0);
+            Assert.AreEqual(firstKiloGramsValue, secondKiloGramsValue);
+        }
+    }
+}
