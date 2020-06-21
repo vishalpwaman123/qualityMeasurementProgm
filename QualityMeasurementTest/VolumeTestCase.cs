1ad156fa52bd5cbd9770a4dc430ac4495309cb7f
unknown <vishalpwaman123@gmail.com>
2020-06-20 23:10:34 -0700

    Added: TC_5.1 Given Two 0 Gallon Check Return Equal

---
+++ QualityMeasurementTest/VolumeTestCase.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+    class VolumeTestCase
+    {
+        private Volumes litre = null;
+        private Volumes gallon = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.litre = new Volumes();
+            this.gallon = new Volumes();
+        }
+
+        [Test]
+        public void Given0GallonAnd0Gallon_ShouldReturnsEqual()
+        {
+            try
+            {
+                double firstGallonValue = gallon.ConvertValueToInch(Volumes.Unit.Gallon, 0.0);
+                double secondGallonValue = gallon.ConvertValueToInch(Volumes.Unit.Gallon, 0.0);
+                Assert.AreEqual(firstGallonValue, secondGallonValue);
+            }
+            catch (QualityMeaurementException e)
+            {
+                Assert.AreEqual(QualityMeaurementException.ExceptionType.INVALID_ARGUMENT, e.Type);
+            }
+        }
+
+    }
+}
