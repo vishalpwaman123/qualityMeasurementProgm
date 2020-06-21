8b84bd16909009bda0a44424a892afc590b46a70
unknown <vishalpwaman123@gmail.com>
2020-06-21 02:44:29 -0700

    Added: Tc8.1_Given 0Fahrenheit and 0Fahrenheit checked Return Equal

---
+++ QualityMeasurementProgram/Temperature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurementProgram
+{
+    public class Temperature
+    {
+        public enum Unit
+        {
+            Fahrenheit,
+            FahrenheitToCelsius
+        }
+
+        public double ConvertValueToCelsius(Unit unit, double value)
+        {
+            try
+            {
+                if (unit.Equals(Unit.FahrenheitToCelsius))
+                {
+                    return value * (100 / 212);
+                }
+
+                return value;
+            }
+            catch (Exception e)
+            {
+                throw new QualityMeaurementException(e.Message, QualityMeaurementException.ExceptionType.INVALID_ARGUMENT);
+            }
+        }
+    }
+}
---
+++ QualityMeasurementTest/TemperatureTestCases.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using QualityMeasurementProgram;
+
+namespace NUnitTestQuantityMeasurement
+{
+    class TemperatureTestCases
+    {
+        private Temperature fahrenheit = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.fahrenheit = new Temperature();
+        }
+
+        [Test]
+        public void Given0FahrenheitAnd0Fahrenheit_ShouldReturnsEqual()
+        {
+            double firstFahrenheitValue = fahrenheit.ConvertValueToCelsius(Temperature.Unit.Fahrenheit, 0.0);
+            double secondFahrenheitValue = fahrenheit.ConvertValueToCelsius(Temperature.Unit.Fahrenheit, 0.0);
+            Assert.AreEqual(firstFahrenheitValue, secondFahrenheitValue);
+        }
+    }
+}
